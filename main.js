e1accba0727b605d85e9f101cc69d2988c5975bb
Jb Ketele <149502977+jbketele@users.noreply.github.com>
2024-01-09 13:24:56 +0100

    carte regions  interactive

--- main.js
+++ main.js
@@ -1,13 +1,1 @@
-const giteCard = document.getElementById("gite_card");
-
-function survol() {
-    giteCard.classList.add('hover-card');
-}
-
-function sortieSurvol() {
-    giteCard.classList.remove('hover-card');
-}
-
-giteCard.addEventListener('mouseover', survol)
-giteCard.addEventListener('mouseout', sortieSurvol)
 
