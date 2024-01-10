f3f34cb0eb91bb172c05b64c110a89e4700e17d1
Jb Ketele <149502977+jbketele@users.noreply.github.com>
2024-01-10 09:07:57 +0100

    ok

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var carte = document.querySelector('.map path');
 
+    hoverableElement.addEventListener('touchstart', function () {
+        carte.classList.add('hovered');
+    });
+
+    hoverableElement.addEventListener('touchend', function () {
+        hoverableElement.classList.remove('hovered');
+    });
+});
