a78d6906cefbc5edc2ffcfc0cc9d71d2674026b6
Jb Ketele <149502977+jbketele@users.noreply.github.com>
2024-02-09 16:30:09 +0100

    chgt font

--- index.php
+++ index.php
@@ -18,9 +18,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital@1&display=swap" rel="stylesheet">
+    
     <link rel="icon" type="image/x-icon" href="img/logo gites detoure.png">
     <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
@@ -34,7 +32,7 @@
     <main>
         <section class="top">
             <div class="container couv" style="padding: 0%;">
-            <h1 class="text-dark text-center texte-couv"><em>Détente/Découverte<br>Dégustation</em></h1>
+            <p class="text-center texte-couv">Détente/Découverte<br>Dégustation</p>
             
             </div>
         </section>
