3a0fa40c077475c243935ee8013e137ab360c27e
Jb Ketele <149502977+jbketele@users.noreply.github.com>
2024-03-20 16:31:21 +0100

    contact

--- views/contact.php
+++ views/contact.php
@@ -20,42 +20,50 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="icon" type="image/x-icon" href="img/logo gites detoure.png">
     <link rel="stylesheet" type="text/css" href="../styles.css">
+    <link rel="stylesheet" href="../sign.css">
+
 </head>
 
 <body>
-<?php require_once'/Applications/MAMP/htdocs/gites_locavores/header-footer/header.php'; ?>
+<?php session_start();
+require_once '/Applications/MAMP/htdocs/gites_locavores/header-footer/header.php'; ?>
 
 
     <main>
         <div class="row">
-            <div class="container" id="contact">
-                <div>
+            <div class="d-flex justify-content-around" id="connect">
 
-                    <div class="container mt-5">
-                        <div class="row justify-content-center">
-                            <div class="col-md-6 d-flex justify-content-center">
-                                <form class="contact" name="contact" method="POST" netlify>
-                                    <p>
-                                        <label>Nom : <input type="text" name="lastname" /></label>
-                                    </p>
-                                    <p>
-                                        <label>Prénom : <input type="text" name="firstname" /></label>
-                                    </p>
-                                    <p>
-                                        <label>Email : <input type="email" name="email" /></label>
-                                    </p>
+                    <div>
+                        <h3 class="mt-3" style="padding: 0">Contact</h3>
+                        <br>
+                            <form class="contact" name="contact" method="POST">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" name="lastname" placeholder="Nom"></input>
+                                    <label for="floatingInputGrid">Nom</label>
+                                </div>
 
-                                    <p>
-                                        <label>Message : <textarea name="message"></textarea></label>
-                                    </p>
-                                    <p class="envoyer">
-                                        <button type="submit">Envoyer</button>
-                                    </p>
-                                </form>
-                            </div>
-                        </div>
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" name="firstname" placeholder="Prénom"></input>
+                                    <label for="floatingInputGrid">Prénom</label>
+                                </div>
+
+                                <div class="form-floating">
+                                    <input type="email" class="form-control" name="email" placeholder="Email"></input>
+                                    <label for="floatingInputGrid">Email</label>
+                                </div>
+
+                                <div class="form-floating">
+                                    <textarea class="form-control" name="message" placeholder="Message"></textarea>
+                                    <label for="floatingInputGrid">Message</label>
+                                </div>
+
+                                <div class="form-floating">
+                                    <input class="btn" type="submit" id="floatingInputGrid" value="Envoyer"></input>
+                                </div>
+                            </form>
+
                     </div>
-                </div>
+                
             </div>
     </main>
 
