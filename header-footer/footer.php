cde189f20770ba66fe3e2b4208ac709efc900c9f
Jb Ketele <149502977+jbketele@users.noreply.github.com>
2024-03-28 14:04:42 +0100

    footer

--- header-footer/footer.php
+++ header-footer/footer.php
@@ -10,7 +10,7 @@
 <footer>
 
         <div class="bg-light border-light shadow py-3">
-            <div class=" follow row">
+            <div class=" follow">
                 <p class="follow container text-center" style=font-size:2rem;>Suivez-nous :
                     <i class="bi bi-facebook" style="font-size: 2rem;"></i>
                     <i class="bi bi-instagram" style="font-size: 2rem;"></i>
@@ -34,13 +34,27 @@
 
 
             <div class="container d-flex flex-wrap row-cols-md-3">
-                <div class="container row offset-1 col-md-2">
-                    <p><a href="gites.php">RECHERCHE DE GÎTES</a></p>
+                <div class="container row ms-2 col-md-4">
+                    <p class="text-center"><a href="regions.php">RECHERCHE DE GÎTES</a></p>
+                    <div class="d-flex p-0">
                         <ul>
-                            <li><a href="hdf.php">Hauts-de-France</a></li>
-                            <li><a href="normandie.php">Normandie</a></li>
+                        <li><a  href="auvergne.php">Auvergne-Rhônes-Alpes</a></li>
+                        <li><a href="bourgogne.php">Bourgogne-Franche-Comté</a></li>
+                        <li><a href="bretagne.php">Bretagne</a></li>
+                        <li><a href="centre.php">Centre-Val de Loire</a></li>
+                        <li><a href="corse.php">Corse</a></li>
+                        <li><a href="grand-est.php">Grand Est</a></li>
                         </ul>
-                   
+                        <ul>
+                        <li><a href="hauts-de-france.php">Hauts de France</a></li>
+                        <li><a href="ile-de-france.php">Ile de France</a></li>
+                        <li><a href="normandie.php">Normandie</a></li>
+                        <li><a href="aquitaine.php">Nouvelle-Aquitaine</a></li>
+                        <li><a href="occitanie.php">Occitanie</a></li>
+                        <li><a href="pays-loire.php">Pays de la Loire</a></li>
+                        <li><a href="paca.php">PACA</a></li>
+                        </ul>
+                   </div>
                 </div>
                 <div class="container row ms-5 col-md-2">
                     <p><a href="idees.php">INSPIREZ-VOUS</a></p>
