66bf009002d669e10fbf9a57f18167496c8b1386
Jb Ketele <149502977+jbketele@users.noreply.github.com>
2024-04-10 16:47:56 +0200

    ok

--- views/martinique.php
+++ views/martinique.php
@@ -12,7 +12,7 @@
     </script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gites Locavores - Guadeloupe</title>
+    <title>Gites Locavores - Martinique</title>
     <link rel="stylesheet" href="bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
